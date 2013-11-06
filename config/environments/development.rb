e6e2de89520844cf03a0349f7637855855210cfd
Jaydeep <jdsingh94@gmail.com>
2013-11-06 13:46:14 -0800

    Reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Mediumlarge::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # config.action_mailer.default_url_options = { :host => 'localhose:3000' }
 end
