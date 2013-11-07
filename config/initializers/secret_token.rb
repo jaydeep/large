10a92137eb2ebe8c7c540775d4385339008d023a
Jaydeep <jdsingh94@gmail.com>
2013-11-07 15:23:12 -0800

    Fixed security flaw with secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mediumlarge::Application.config.secret_token = '7e68f94275a57cc0fd0b64a2bb0b1b5f6fb9647edd1ed8950f34d16506e689170703e167c33c2d5d099f1869bbb7554c19f191f6ccd33db5b830947ca6e2b500'
+Mediumlarge::Application.config.secret_token = ENV['SECRET_TOKEN']
