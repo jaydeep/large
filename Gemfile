66730dcc7a0043b13e5d0bcf9628375e832d0e2d
Jaydeep <jdsingh94@gmail.com>
2013-11-08 17:22:06 -0800

    Backbone Init

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'pg'
+gem 'backbone-on-rails'
 
 group :development do 
   gem 'rails-pry'
---
+++ app/assets/javascripts/mediumlarge.js
@@ -0,0 +1,13 @@
+window.Mediumlarge = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Mediumlarge.initialize();
+});
