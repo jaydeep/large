69f58df4ffa971e645bc57fd000e1544ee893672
Jaydeep <jdsingh94@gmail.com>
2013-11-20 17:33:21 -0800

    Add marionette

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require bootstrap
 //= require underscore
 //= require backbone
+//= require backbone.marionette
 //= require mediumlarge
 //= require_tree ../templates
 //= require_tree ./models
